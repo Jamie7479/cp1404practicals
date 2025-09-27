05d14ca13d75576a01b3695695f0b29a4c0cf6e9
jamie <jawwhite11@gmail.com>
2025-09-27 15:47:55 +1000

    Finish score menu program

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -4,16 +4,39 @@ MENU = """(G)et a valid score
 (S)how stars
 (Q)uit"""
 
-print(MENU)
-choice = input("->").upper()
-while choice != "Q":
-    if choice == "P":
-        <do first task>
-    elif choice == "S"
-        <do second task>
-    else
-        print("Invalid input")
+
+def main():
     print(MENU)
     choice = input("->").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            print(determine_grade(score))
+        elif choice == "S":
+            print("*" * score)
+        else:
+            print("Invalid input")
+        print(MENU)
+        choice = input("->").upper()
 
-print("Farewell")
+    print("Farewell")
+
+def get_valid_score():
+    score = int(input("Score: "))
+    while score < 0 or score > 100:
+        score = int(input("Score: "))
+    return score
+
+def determine_grade(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
