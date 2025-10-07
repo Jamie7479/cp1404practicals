ee74ddcc22fa875b0f5f3a94457edf56e4bbb8ee
jamie <jawwhite11@gmail.com>
2025-10-07 13:00:40 +1000

    Autoformat quick picks program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -12,7 +12,7 @@ number_of_quick_picks = int(input("How many quick picks? "))
 for i in range(number_of_quick_picks):
     line = []
     for j in range(6):
-        new_number = random.randint(MIN,MAX)
+        new_number = random.randint(MIN, MAX)
         while new_number in line:
             new_number = random.randint(MIN, MAX)
         line.append(new_number)
