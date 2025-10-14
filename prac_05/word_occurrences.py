b5a1663cf6decef90be2f1c7c4f6e51e1115fec7
jamie <jawwhite11@gmail.com>
2025-10-14 13:52:00 +1000

    Add kivy demo program and layout

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,10 +4,10 @@ Estimate: 10 minutes
 Actual:   5 minutes
 """
 
-text = input("Text: ")
+
 word_to_word_count = {}
 
-for word in text.split():
+for word in input("Text: ").split():
     word_to_word_count[word] = word_to_word_count.get(word, 0) + 1
 
 max_word_length = max(len(word) for word in word_to_word_count)
