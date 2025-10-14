84cba6f334985027e507b3054ec88e97a413e17b
jamie <jawwhite11@gmail.com>
2025-10-14 13:26:30 +1000

    Add and complete prac 5 emails exercise

---
+++ prac_05/emails.py
@@ -0,0 +1,20 @@
+"""
+Word Occurrences
+Estimate: 10 minutes
+Actual:   12 minutes
+"""
+
+email_to_name = {}
+
+email = input("Email: ")
+while email != "":
+    name = " ".join(email.split("@")[0].split(".")).title()
+    choice = input(f"Is your name {name}? (Y/n) ").lower()
+    if choice == "y" or choice == "":
+        email_to_name[email] = name
+    else:
+        email_to_name[email] = input("Name: ")
+    email = input("Email: ")
+
+for email, name in email_to_name.items():
+    print(f"{name} ({email})")
