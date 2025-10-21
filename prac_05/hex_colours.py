785f5c5e835c9a6c9258fdb522e4b3b3d60d3456
Jamie White <jamie.white1@my.jcu.edu.au>
2025-10-21 17:26:38 +1000

    Use .title() instead of .lower() to match colours to hex codes to improve output appearance

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,15 +3,15 @@ CP1404/CP5632 Practical
 Hex values in a dictionary, with english keys
 """
 
-NAME_TO_HEX = {"alizarincrimson": "#e32636", "amber": "#ffbf00", "amethyst": "#9966cc", "asparagus": "#87a96b", "babypink": "#f4c2c2",
-               "boysenberry": "#873260", "bronze": "#cd7f32", "burgundy": "#800020", "burntsienna": "#e97451", "cadmiumgreen": "#006b3c"}
+NAME_TO_HEX = {"Alizarin Crimson": "#e32636", "Amber": "#ffbf00", "Amethyst": "#9966cc", "Asparagus": "#87a96b", "Baby Pink": "#f4c2c2",
+               "Boysenberry": "#873260", "Bronze": "#cd7f32", "Burgundy": "#800020", "Burnt Sienna": "#e97451", "Cadmium Green": "#006b3c"}
 
 print(NAME_TO_HEX)
 
-name = input("Enter colour name: ").lower()
+name = input("Enter colour name: ").title()
 while name != "":
     try:
         print(f"{name} is {NAME_TO_HEX[name]}")
     except KeyError:
         print("Invalid colour name")
-    name = input("Enter colour name: ").lower()
+    name = input("Enter colour name: ").title()
