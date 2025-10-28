05113ac72d92c697d4cc12e922327de780d77788
Jamie White <jamie.white1@my.jcu.edu.au>
2025-10-28 12:43:20 +1000

    Add ProgrammingLanguage class docstrings

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -6,16 +6,21 @@ End time: 12.40
 Duration: 8mins
 """
 
+
 class ProgrammingLanguage:
-    """"""
+    """Create programming language instance"""
+
     def __init__(self, name, typing, reflection, year):
+        """Initialise programming language instance"""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
+        """Return string summary"""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
+        """Determines if language instance is dynamic"""
         return self.typing == "Dynamic"
