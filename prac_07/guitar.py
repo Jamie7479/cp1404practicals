afec9a42118adc7c3954f5d9582332f5b68c92c0
Jamie White <jamie.white1@my.jcu.edu.au>
2025-11-08 22:53:43 +1000

    Add dog strings to guitar class methods

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -12,20 +12,25 @@ class Guitar:
     CURRENT_YEAR = 2025
 
     def __init__(self, name="", year=0, cost=0):
+        """Initialise guitar fields"""
         self.name = name
         self.year = int(year)
         self.cost = float(cost)
 
     def __str__(self):
+        """Define string representation"""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
     def __lt__(self, other):
+        """Determine if self year is less than other year"""
         return self.year < other.year
 
     def get_age(self):
+        """Return age of guitar"""
         return 2025 - self.year
 
     def is_vintage(self):
+        """Determine if guitar is vintage"""
         return self.get_age() >= 50
 
 
