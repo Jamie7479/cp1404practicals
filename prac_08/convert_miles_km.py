935372fd424869f461b7e1b099276f42014cda69
Jamie White <jamie.white1@my.jcu.edu.au>
2025-11-11 12:05:26 +1000

    Complete prac 8 stage 1 of miles to km gui base

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,10 +1,29 @@
+"""
+Convert miles to km GUI
+"""
+
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
 
 class ConvertMilesKm(App):
+    output_string = StringProperty()
+
     def build(self):
+        """Build app"""
         self.title = 'Convert Miles to Kilometres'
         self.root = Builder.load_file('convert_miles_km.kv')
+        self.output_string = '54.71756'
         return self.root
+
+    def handle_convert(self):
+        """Convert miles to km and change output based on text input"""
+        self.output_string = str(float(self.root.ids.user_input.text) * 1.60934)
+
+    def handle_increment(self, increment):
+        """Increment text input"""
+        self.root.ids.user_input.text = str(float(self.root.ids.user_input.text) + increment)
+
 
 ConvertMilesKm().run()
